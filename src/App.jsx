a6e293a93626f980f8b6e7aec8efa0195b20fc2b
anas mohammed <anasmohammed@MacBook-Air-anas.local>
2026-04-15 14:02:53 +0200

    complite the hero section and start to TrustedBy component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import TrustedBy from './components/TrustedBy'
 
 
 const App = () => {
-  const [theme , setTheme] = useState(localStorage.getItem("theme") ?? "light")
-  
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "light")
+
   return (
     <div className="relative min-h-dvh w-full bg-zinc-50 text-zinc-900 dark:bg-zinc-950 dark:text-zinc-100 transition-all duration-300">
       <Navbar theme={theme} setTheme={setTheme} />
-   <Hero/>
+      <Hero />
+      <TrustedBy/>
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,8 +15,11 @@ dark:text-white'>
             <div>
                 <h1 className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium
                  xl:leading-[95px] max-w-5xl'>Turning imagination into <span className='bg-gradient-to-r from-[#5044E5] to-[#4d8cea] bg-clip-text text-transparent'>digital</span> impact.</h1>
-                <p className='text-center text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-[80%] sm:max-w-lg pb-3'>Creating meaningful connections and turning big ideas into interactive digital experiences.</p>
-
+                <p className='mt-4 text-center text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-[80%] sm:max-w-lg pb-3 mx-auto'>Creating meaningful connections and turning big ideas into interactive digital experiences.</p>
+            </div>
+            <div className="relative w-full">
+                <img src={assets.hero_img} alt="hero img" className='w-ful max-w-6xl' />
+                <img src={assets.bgImage1} alt="bgcolor" className='absolute -top-40 z-1 -right-40 sm:-top-100 sm:-right-70 dark:hidden' />
             </div>
         </div>
     )
